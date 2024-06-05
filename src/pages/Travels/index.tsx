6dd6a1114d9a51444f06d0bd1fefa5201073e882
Diego <drobert1403@gmail.com>
2024-06-05 11:19:41 -0300

    test/implementation of socket-io by diego ;)

--- src/pages/Travels/index.tsx
+++ src/pages/Travels/index.tsx
@@ -1,43 +1,51 @@
 import { useState, useEffect } from 'react';
 import useWebSocket, {ReadyState} from 'react-use-websocket';
+import { initiateSocket, subscribeToChat } from './socket_util';
+import { subscribe } from 'diagnostics_channel';
 
 const Travels = () => {
   const [data, setData] = useState(null);
 
-  // URL del WebSocket
-  const socketUrl = 'ws://localhost:1000';
+  // // URL del WebSocket
+  // const socketUrl = 'http://localhost:1000';
 
-  // Usar el hook useWebSocket
-  const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
+  // // Usar el hook useWebSocket
+  // const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
 
-  // Efecto para manejar los mensajes recibidos
-  useEffect(() => {
-    if (lastMessage !== null) {
-      try {
-        const eventData = JSON.parse(lastMessage?.data);
-        if (eventData.event === 'front/031054167945') {
-          setData(eventData.data);
-        }
+  // // Efecto para manejar los mensajes recibidos
+  // useEffect(() => {
+  //   if (lastMessage !== null) {
+  //     try {
+  //       const eventData = JSON.parse(lastMessage?.data);
+  //       if (eventData.event === 'front/031054167945') {
+  //         setData(eventData.data);
+  //       }
 
-      } catch (error) {
-        console.error('Error parsing JSON:', error);
-      }
-    }
-  }, [lastMessage]);
+  //     } catch (error) {
+  //       console.error('Error parsing JSON:', error);
+  //     }
+  //   }
+  // }, [lastMessage]);
 
-  // Estado del WebSocket
-  const connectionStatus = {
-    [ReadyState.CONNECTING]: 'Connecting',
-    [ReadyState.OPEN]: 'Open',
-    [ReadyState.CLOSING]: 'Closing',
-    [ReadyState.CLOSED]: 'Closed',
-    [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
-  }[readyState];
+  // // Estado del WebSocket
+  // const connectionStatus = {
+  //   [ReadyState.CONNECTING]: 'Connecting',
+  //   [ReadyState.OPEN]: 'Open',
+  //   [ReadyState.CLOSING]: 'Closing',
+  //   [ReadyState.CLOSED]: 'Closed',
+  //   [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+  // }[readyState];
 
+  useEffect(()=>{
+    initiateSocket('messageToServer')
+    subscribeToChat((error, msg)=>{
+        setData(msg);
+    })
+  },[])
   return (
     <div>
-      <p>Connection Status: {connectionStatus}</p>
-      <p>Received Data: {data}</p>
+      {/* <p>Connection Status: {connectionStatus}</p> */}
+      <p>Received Data: {JSON.stringify(data)}</p>
     </div>
   );
 };
